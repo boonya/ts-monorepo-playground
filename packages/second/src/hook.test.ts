d852ec25bfa1b7e5fbf65a92ccedee4de24a46fd
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-08-03 16:33:17 +0200

    braking change

--- packages/second/src/hook.test.ts
+++ packages/second/src/hook.test.ts
@@ -1,9 +1,9 @@
 import hook from './hook';
 
 it('should return default value.', () => {
-  expect(hook()).toBe("I am a hook that returns a word \"-\".");
+  expect(hook()).toBe("I am a hook that returns a word \"-\". And I am changed a bit.");
 })
 
 it('should return specific value.', () => {
-  expect(hook('WoRD')).toBe("I am a hook that returns a word \"WoRD\".");
+  expect(hook('WoRD')).toBe("I am a hook that returns a word \"WoRD\". And I am changed a bit.");
 })
