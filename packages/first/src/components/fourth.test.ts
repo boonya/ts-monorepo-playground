f0753291c19db00cf8c852ff32054b4aea3df819
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-08-03 16:27:56 +0200

    New release (#10)
    * clean up
    * additional test

---
+++ packages/first/src/components/fourth.test.ts
@@ -0,0 +1,7 @@
+import fourth from '@boonya/ts-monorepo-playground-first/components/fourth';
+
+it('fourth test', () => {
+  const result = fourth('This word');
+
+  expect(result).toBe(`I am a hook that returns a word "This word".`);
+});
